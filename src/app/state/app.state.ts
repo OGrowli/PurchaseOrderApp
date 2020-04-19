d464bdf34660a4ea7d855a5aff8e4ff2bbe27ed4
unknown <seamusodunn@gmail.com>
2020-04-19 11:07:36 -0300

    added app state

---
+++ src/app/state/app.state.ts
@@ -0,0 +1,9 @@
+import { Params, Data} from '@angular/router';
+import { createFeatureSelector } from '@ngrx/store';
+
+export interface State {
+}
+
+export enum Features {
+    PurchaseOrders = 'purchaseOrders'
+}
